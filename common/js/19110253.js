c35d7771a0370516912958f634d4d7f69ec15389
xiaoHn <973974776@qq.com>
2017-07-13 11:49:41 +0800

    南部漂泊
    laimooc

---
+++ common/js/19110253.js
@@ -0,0 +1,11 @@
+document.write ('<a href="http://www.51.la/?19110253" target="_blank" title="51.La &#x7F51;&#x7AD9;&#x6D41;&#x91CF;&#x7EDF;&#x8BA1;&#x7CFB;&#x7EDF;">&#x7F51;&#x7AD9;&#x7EDF;&#x8BA1;</a>\n');
+var a0253tf="51la";var a0253pu="";var a0253pf="51la";var a0253su=window.location;var a0253sf=document.referrer;var a0253of="";var a0253op="";var a0253ops=1;var a0253ot=1;var a0253d=new Date();var a0253color="";if (navigator.appName=="Netscape"){a0253color=screen.pixelDepth;} else {a0253color=screen.colorDepth;}
+try{a0253tf=top.document.referrer;}catch(e){}
+try{a0253pu =window.parent.location;}catch(e){}
+try{a0253pf=window.parent.document.referrer;}catch(e){}
+try{a0253ops=document.cookie.match(new RegExp("(^| )a0253_pages=([^;]*)(;|$)"));a0253ops=(a0253ops==null)?1: (parseInt(unescape((a0253ops)[2]))+1);var a0253oe =new Date();a0253oe.setTime(a0253oe.getTime()+60*60*1000);document.cookie="a0253_pages="+a0253ops+ ";path=/;expires="+a0253oe.toGMTString();a0253ot=document.cookie.match(new RegExp("(^| )a0253_times=([^;]*)(;|$)"));if(a0253ot==null){a0253ot=1;}else{a0253ot=parseInt(unescape((a0253ot)[2])); a0253ot=(a0253ops==1)?(a0253ot+1):(a0253ot);}a0253oe.setTime(a0253oe.getTime()+365*24*60*60*1000);document.cookie="a0253_times="+a0253ot+";path=/;expires="+a0253oe.toGMTString();}catch(e){}
+try{if(document.cookie==""){a0253ops=-1;a0253ot=-1;}}catch(e){}
+a0253of=a0253sf;if(a0253pf!=="51la"){a0253of=a0253pf;}if(a0253tf!=="51la"){a0253of=a0253tf;}a0253op=a0253pu;try{lainframe}catch(e){a0253op=a0253su;}
+a0253src='http://web.51.la:82/go.asp?svid=15&id=19110253&tpages='+a0253ops+'&ttimes='+a0253ot+'&tzone='+(0-a0253d.getTimezoneOffset()/60)+'&tcolor='+a0253color+'&sSize='+screen.width+','+screen.height+'&referrer='+escape(a0253of)+'&vpage='+escape(a0253op)+'&vvtime='+a0253d.getTime();
+setTimeout('a0253img = new Image;a0253img.src=a0253src;',0);
+document.write ('<script language="javascript" type="text/javascript" src="http://hello.51.la/?id=19110253"></script>');
